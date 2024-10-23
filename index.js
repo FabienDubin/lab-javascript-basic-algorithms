250bdc2f5f6dd98d64738fd4c6fe724d3ce24f4e
Fabien Dubin <fabien.dubin@gmail.com>
2024-10-23 15:10:16 +0200

    Job partlly done with Edvige

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 // Iteration 1: Names and Input
 
+const hacker1 = "Fabien";
+console.log(`The driver's name is ${hacker1}`);
+
+const hacker2 = "Edvi";
+
+console.log(`The navigator name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+if (hacker1.length === hacker2.length) {
+  console.log(
+    `Wow, you both have equally long names, ${hacker2.length} characters!`
+  );
+} else if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+} else {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
+}
 
 // Iteration 3: Loops
+
+let finalName = "";
+
+for (let i = 0; i < hacker1.length; i++) {
+  finalName += hacker1[i] + " ";
+  finalName = finalName.toUpperCase;
+}
+
+console.log(finalName);
